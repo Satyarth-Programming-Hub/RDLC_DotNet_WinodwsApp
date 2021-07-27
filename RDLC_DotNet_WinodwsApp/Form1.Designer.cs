f11984f9af315ba5e88e8fbd8830e8ac3b7122fb
TechTalkSaty <noreply@example.com>
2021-07-27 12:09:15 +0530

    Add project files.

---
+++ RDLC_DotNet_WinodwsApp/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+namespace RDLC_DotNet_WinodwsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCountry = new System.Windows.Forms.TextBox();
+            this.btnLoadReport = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "RDLC_DotNet_WinodwsApp.Reports.EmployeesDetails.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(-4, 70);
+            this.reportViewer1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(1319, 612);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(80, 17);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Country";
+            // 
+            // txtCountry
+            // 
+            this.txtCountry.Location = new System.Drawing.Point(199, 17);
+            this.txtCountry.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtCountry.Name = "txtCountry";
+            this.txtCountry.Size = new System.Drawing.Size(246, 27);
+            this.txtCountry.TabIndex = 2;
+            // 
+            // btnLoadReport
+            // 
+            this.btnLoadReport.Location = new System.Drawing.Point(521, 14);
+            this.btnLoadReport.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnLoadReport.Name = "btnLoadReport";
+            this.btnLoadReport.Size = new System.Drawing.Size(135, 33);
+            this.btnLoadReport.TabIndex = 3;
+            this.btnLoadReport.Text = "Load Report";
+            this.btnLoadReport.UseVisualStyleBackColor = true;
+            this.btnLoadReport.Click += new System.EventHandler(this.btnLoadReport_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1329, 691);
+            this.Controls.Add(this.btnLoadReport);
+            this.Controls.Add(this.txtCountry);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.reportViewer1);
+            this.Font = new System.Drawing.Font("Bookman Old Style", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCountry;
+        private System.Windows.Forms.Button btnLoadReport;
+    }
+}
+
---
+++ RDLC_DotNet_WinodwsApp/Form1.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Reporting.WinForms;
+using RDLC_DotNet_WinodwsApp.DataSets;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RDLC_DotNet_WinodwsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer1.RefreshReport();
+        }
+
+        private void btnLoadReport_Click(object sender, EventArgs e)
+        {
+            if (txtCountry.Text == "" || txtCountry.Text == null)
+            {
+                MessageBox.Show("Parameter is required!!!!");
+            }
+            else
+            {
+                this.reportViewer1.ProcessingMode = ProcessingMode.Local;
+                var employees = GetReportData();
+                var dataSource = new ReportDataSource("Employees", employees.Tables[0]);
+                this.reportViewer1.LocalReport.DataSources.Clear();
+                this.reportViewer1.LocalReport.DataSources.Add(dataSource);
+                this.reportViewer1.RefreshReport();
+
+            }
+        }
+
+        private Employees GetReportData()
+        {
+            string constr = "Data Source = LAPTOP-TG14SPMU; Initial Catalog = SSRSDB; Integrated Security = SSPI;";
+            string query = "SELECT * FROM Employee WHERE country=@Country";
+            var cmd = new SqlCommand(query);
+            cmd.Parameters.AddWithValue("@Country", this.txtCountry.Text);
+            using (var con = new SqlConnection(constr))
+            {
+                using (var da = new SqlDataAdapter())
+                {
+                    cmd.Connection = con;
+                    da.SelectCommand = cmd;
+                    using (var employees = new Employees())
+                    {
+                        da.Fill(employees, "Employee");
+                        return employees;
+                    }
+                }
+            }
+        }
+    }
+}
